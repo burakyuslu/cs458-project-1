26c9de4bac2a13021d341c4a4b9e73f4f42be98b
Elif Kurtay <elif.kurtay00@gmail.com>
2022-02-12 22:22:08 +0300

    firebase setup

--- netflix-login/src/App.js
+++ netflix-login/src/App.js
@@ -1,10 +1,80 @@
 import './App.css';
 
 function App() {
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '{your-app-id}',
+          cookie     : true,
+          xfbml      : true,
+          version    : '{api-version}'
+        });
+
+        FB.AppEvents.logPageView();
+
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "https://connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
   return (
      <>
+
+        <fb:login-button
+          scope="public_profile,email"
+          onlogin="checkLoginState();">
+        </fb:login-button>
      </>
     );
 }
 
 export default App;
+
+/* Facebook SDK
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+
+    FB.AppEvents.logPageView();
+
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+CHECK LOGIN
+
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+Response:
+{
+    status: 'connected',
+    authResponse: {
+        accessToken: '...',
+        expiresIn:'...',
+        signedRequest:'...',
+        userID:'...'
+    }
+}
+*/
---
+++ netflix-login/src/database/firebase.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQrHB_NS7kyK-yE_MrY_H_OorqCSeC_Nw",
+  authDomain: "netflix-login-931d9.firebaseapp.com",
+  projectId: "netflix-login-931d9",
+  storageBucket: "netflix-login-931d9.appspot.com",
+  messagingSenderId: "983589309155",
+  appId: "1:983589309155:web:684d23091c73ac79d5d3d6",
+  measurementId: "G-4EWXSK1XXH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Get a list of users from database
+async function getUsers(db) {
+  const usersCol = collection(db, 'users');
+  const usersSnapshot = await getDocs(usersCol);
+  const userList = usersSnapshot.docs.map(doc => doc.data());
+  return userList;
+}
+
+const auth = getAuth();
+
+signInWithEmailAndPassword(auth, email, password)
+.then((userCredential) => {
+ // Signed in
+ const user = userCredential.user;
+ // ...
+})
+.catch((error) => {
+ const errorCode = error.code;
+ const errorMessage = error.message;
+});
