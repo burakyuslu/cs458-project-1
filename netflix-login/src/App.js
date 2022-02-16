a617e262232c91e3a148acaf7cc3f08a74321248
pelin <celikspzpelin@gmail.com>
2022-02-16 23:09:59 +0300

    user login with email is completed

--- netflix-login/src/App.js
+++ netflix-login/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import {Button, Checkbox, FormControlLabel, FormGroup, Grid, Paper, TextField} from "@mui/material";
 import background from "./images/background-img.jpg";
-import logo  from "./images/newlogo.png";
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import {auth} from "./database/firebase-config";
+import {signInWithEmailAndPassword} from "firebase/auth";
 
 function App() {
+
+   const[loginEmail, setLoginEmail] = useState("");
+   const[loginPassword, setLoginPassword] = useState("");
+
+   const login = async () => {
+      try{
+         const user = signInWithEmailAndPassword(
+             auth,
+             loginEmail,
+             loginPassword
+         )
+         console.log(user);
+      } catch (error) {
+         console.log(error.message());
+      }
+   }
 
    return (
       <div style={{ backgroundImage: `url(${background})` }} >
@@ -31,9 +48,9 @@ function App() {
             <Grid style={{ paddingTop: '8%', marginBottom: '10%'}}  container xs={2}>
                <Paper style = {{ backgroundColor: 'rgba(0, 0, 0, 0.8)'}} container variant="outlined" elevation={3}>
                   <h2 style={{ color: "white", paddingTop: '2%', paddingLeft: '5%'}} > Oturum Aç </h2>
-                  <TextField style={{ marginTop: '2%', marginLeft: '5%', minWidth:'90%', backgroundColor:'lightgray' }} id="filled-basic" label="E-posta veya telefon numarası" variant="filled"/>
-                  <TextField style={{ marginTop: '2%', marginLeft: '5%', minWidth:'90%', backgroundColor:'lightgray'}} id="filled-basic" label="Parola" variant="filled" />
-                  <Button style={{ marginTop: '2%', marginLeft: '5%', minWidth:'90%', backgroundColor: "#ff0000 ",}} variant="contained">Oturum Aç</Button>
+                  <TextField style={{ marginTop: '2%', marginLeft: '5%', minWidth:'90%', backgroundColor:'lightgray' }} id="filled-basic" label="E-posta veya telefon numarası" variant="filled" onChange={(event) => setLoginEmail(event.target.value)}/>
+                  <TextField style={{ marginTop: '2%', marginLeft: '5%', minWidth:'90%', backgroundColor:'lightgray'}} id="filled-basic" label="Parola" variant="filled" onChange={(event) => setLoginPassword(event.target.value)}/>
+                  <Button  onClick={login} style={{ marginTop: '2%', marginLeft: '5%', minWidth:'90%', backgroundColor: "#ff0000 ",}} variant="contained">Oturum Aç</Button>
 
                   <FormGroup style = {{marginLeft: '5%', maxWidth: '50%', color:'white'}}>
                      <FormControlLabel  control={<Checkbox style = {{ color: "#ff0000 "}} defaultChecked />} label="Beni Hatırla" />
